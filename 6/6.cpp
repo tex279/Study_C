d47e26d17f62bc922f1afb834175a321a0c32369
Andeo1812 <alexanforall@gmail.com>
2022-02-09 13:30:03 +0300

    6 coplited...(50/50)

--- 6/6.cpp
+++ 6/6.cpp
@@ -2,12 +2,14 @@
 #include <iostream>
 #include <algorithm>
 #include <string>
+#include <vector>
 
 #define MIN_COUNT 1
 #define MAX_COUNT 3000
 
 #define MIN_TIME 1
 #define MAX_TIME 1000
+
 
 
 #define ERROR_INPUT -1
@@ -26,6 +28,9 @@ public:
     int input_parameters();
 
     void get_res_count();
+
+    size_t check_max_length(char type);
+    size_t check_count_realise();
 
     CheckCommit();
     ~CheckCommit() = default;
@@ -69,43 +74,81 @@ int CheckCommit::input_combination() {
 CheckCommit::CheckCommit() {
     input_parameters();
     input_combination();
+
+    //std::cout << count << " " << short_t << " " << long_t << "\n" << combination << std::endl;
 }
 
-void CheckCommit::get_res_count() {
+size_t CheckCommit::check_max_length(char type) {
+    size_t max_time = 0;
+    bool flag_base_type = false;
+    bool flag_second_type = false;
+    size_t max_count_type = 0;
+    size_t max_count_type_local = 0;
+
+    size_t time;
+
+    char second_type;
+
+    if (type == 'B') {
+        second_type = 'S';
+        time = long_t;
+    } else if (type == 'S') {
+        second_type = 'B';
+        time = short_t;
+    }
+
+    for (size_t i = 0; i < count; i++ ) {
+        if (flag_base_type == true) {
+            max_count_type_local++;
+        }
+
+        if (combination[i] == type && flag_base_type == false) {
+            flag_base_type = true;
+            flag_second_type = false;
+
+            max_count_type_local++;
+        }
+
+        if (combination[i] == second_type && flag_second_type == false) {
+            flag_second_type = true;
+            flag_base_type = false;
+
+            max_count_type_local = 0;
+        }
+
+        if (max_count_type_local > max_count_type) {
+            max_count_type = max_count_type_local;
+        }
+    }
+
+
+    for (size_t i = 0; i < max_count_type; i++) {
+        max_time += time;
+    }
+
+    return max_time;
+}
+
+size_t CheckCommit::check_count_realise() {
     size_t max_time = 0;
     bool flag_B = false;
     bool flag_S = false;
-    size_t max_count_B = 0;
-    size_t max_count_B_local = 0;
 
     std::string zip_combination;
 
-    std::cout << count << " " << short_t << " " << long_t << "\n" << combination << std::endl;
-
     for (size_t i = 0; i < count; i++ ) {
-        if (flag_B == true) {
-            max_count_B_local++;
-        }
-
         if (combination[i] == 'B' && flag_B == false) {
             zip_combination.push_back('B');
             flag_B = true;
             flag_S = false;
-
-            max_count_B_local++;
         }
 
         if (combination[i] == 'S' && flag_S == false) {
             zip_combination.push_back('S');
             flag_S = true;
             flag_B = false;
-
-            max_count_B_local = 0;
         }
 
-        if (max_count_B_local > max_count_B) {
-            max_count_B = max_count_B_local;
-        }
     }
 
     for (size_t i = 0; i < zip_combination.size(); i++) {
@@ -118,15 +161,15 @@ void CheckCommit::get_res_count() {
         max_time += short_t;
     }
 
-    if (max_count_B > 2) {
-        for (size_t i = 0; i < max_count_B - 2; i++) {
-            max_time += long_t;
-        }
-    }
+    return max_time;
+}
 
-    std::cout << max_count_B << std::endl;
-    std::cout << zip_combination << std::endl;
-    std::cout << max_time << std::endl;
+void CheckCommit::get_res_count() {
+    size_t max = std::max(check_max_length('B'), check_max_length('S'));
+    max = std::max(max, check_count_realise());
+
+    std::cout << max << std::endl;
+
 }
 
 
