75b359db4dc3595043b378f28a2289aec3ae87aa
Andeo1812 <alexanforall@gmail.com>
2022-03-18 02:47:36 +0300

    CI

--- run_memtest.sh
+++ run_memtest.sh
@@ -9,4 +9,4 @@ NO_LOST=$(grep "${NO_LOST_PATTERN}" "${VALGRIND_LOG}")
 NO_ERROR=$(grep "${NO_ERROR_PATTERN}" "${VALGRIND_LOG}")
 if [ -z "${NO_LOST}" ] || [ -z "${NO_ERROR}" ]; then cat "${VALGRIND_LOG}"; exit 1; fi
 
-cat
+cat ${VALGRIND_LOG}
