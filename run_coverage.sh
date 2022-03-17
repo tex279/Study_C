7f87c957894ca1ebc34a4a0773b21506bc78f25d
Andeo1812 <alexanforall@gmail.com>
2022-03-18 02:52:08 +0300

    CI

--- run_coverage.sh
+++ run_coverage.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-LOG_TEST="test.log"
+LOG_TEST="../test.log"
 
 cd build
 
 ./test.out
 lcov -t "test.out" -o coverage.info -c -d CMakeFiles/lib.dir/
-(genhtml -o report coverage.info) > "${LOG_TEST}"
+(genhtml -o report coverage.info --output-directory ../coverage-report/) > "${LOG_TEST}"
 
 cat ${LOG_TEST}
