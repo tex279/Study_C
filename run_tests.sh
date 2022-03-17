97cb909bc3cb046a488bf390c5c00c328cac2c62
Andeo1812 <alexanforall@gmail.com>
2022-03-17 21:14:57 +0300

    infro base yes

--- run_tests.sh
+++ run_tests.sh
@@ -2,5 +2,5 @@
 
 cd build
 ./test.out
-lcov -t "test.out" -o coverage.info -c -d
+lcov -t "test.out" -o coverage.info -c -d /
 genhtml -o report coverage.info --output-directory ../coverage-report/
