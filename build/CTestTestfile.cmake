1ee4ce30922675e098a7df78ca43cfc5ad349623
Andeo1812 <alexanforall@gmail.com>
2022-03-18 03:13:28 +0300

    CI - build

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/andeo/GitHub/Study_C_CPP
-# Build directory: /home/andeo/GitHub/Study_C_CPP/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
