22763a8e6ebc2562254e311429e8488643ea447e
Andeo1812 <alexanforall@gmail.com>
2022-05-05 12:24:10 +0300

    1 in process

--- module_2/1.cpp
+++ module_2/1.cpp
@@ -14,7 +14,7 @@ public:
     StringHasher() = default;
     ~StringHasher() = default;
 
-    size_t operator()(const std::string &str);
+    size_t operator()(const std::string &str, const size_t prime);
 };
 
 size_t StringHasher::operator()(const std::string &str, const size_t prime) {
@@ -46,11 +46,15 @@ struct HashTableNode {
 
     status_node status;
 
-    HashTableNode() : status(NIL);
+    HashTableNode() : status(NIL) {};
 
-    HashTableNode(const T &data) : data(data), next(KEY) {};
+    HashTableNode(const T &data) : data(data), status(KEY) {};
 
     ~HashTableNode() = default;
+
+    friend std::ostream& operator<<(std::ostream &os, const HashTableNode<T> &it)  {
+        return os << it.data << " " << it.status << std::endl;
+    }
 };
 
 template<typename T, typename Probing>
@@ -72,34 +76,110 @@ public:
 
     bool Search(const T &key);
 
-    HashTable(const size_t initial_capacity = INITIAL_CAPACITY) : table(initial_capacity, nullptr), size(0) {};
-
+    HashTable(const size_t initial_capacity = INITIAL_CAPACITY) : table(initial_capacity), size(0) {};
     ~HashTable() = default;
+
+    friend std::ostream& operator<<(std::ostream &os, const HashTable<T, Probing> &it)  {
+        for (auto &value : it.table) {
+            os << value.data << " " << value.status << std::endl;
+        }
+
+        os << it.size << " " << it.table.size() << std::endl;
+
+        return os;
+    }
 };
 
 template<typename T, typename Hasher>
 bool HashTable<T, Hasher>::IsFull() const {
-    return size == table.size() * MAX_ALPHA;
+    //  std::cout << size << "!!!! !!!!" << table.size() << std::endl;
+    return size >= table.size() * MAX_ALPHA;
 }
 
 template<typename T, typename Hasher>
 bool HashTable<T, Hasher>::Add(const T &key) {
+    if (IsFull()) {
+        //  std::cout << size << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << table.size() << std::endl;
+        Resize();
+    }
 
+    ssize_t pos_DEL = -1;
+    for (size_t i = 0; i < table.size(); ++i) {
+        size_t hash = probing(key, i) % table.size();
+
+        if (pos_DEL == -1 && table[hash].status == DEL) {
+            pos_DEL = hash;
+        }
+
+        if (table[hash].status == NIL) {
+            if (pos_DEL != -1) {
+                table[pos_DEL].data = key;
+                table[pos_DEL].status = KEY;
+            } else {
+                table[hash].data = key;
+                table[hash].status = KEY;
+            }
+
+            ++size;
+
+            return true;
+        }
+
+        if (table[hash].data == key) {
+            return false;
+        }
+    }
+
+    table[pos_DEL].data = key;
+
+    ++size;
+
+    return true;
 }
 
 template<typename T, typename Hasher>
 bool HashTable<T, Hasher>::Delete(const T &key) {
+    for (size_t i = 0; i < table.size(); ++i) {
+        size_t hash = probing(key, i) % table.size();
 
+        if (table[hash].status == NIL) {
+            return false;
+        }
+
+        if (table[hash].data == key) {
+            table[hash].data = {};
+
+            table[hash].status = DEL;
+
+            --size;
+
+            return true;
+        }
+    }
+
+    return false;
 }
 
 template<typename T, typename Hasher>
 bool HashTable<T, Hasher>::Search(const T &key) {
+    for (size_t i = 0; i < table.size(); ++i) {
+        size_t hash = probing(key, i) % table.size();
 
+        if (table[hash].status == NIL) {
+            return false;
+        }
+
+        if (table[hash].data == key) {
+            return true;
+        }
+    }
+
+    return false;
 }
 
 template<typename T, typename Hasher>
 void HashTable<T, Hasher>::Resize() {
-    std::vector <HashTableNode<T>> new_table(this->table.size() * 2, nullptr);
+    std::vector <HashTableNode<T>> new_table(this->table.size() * 2);
 
     std::vector <HashTableNode<T>> buf = std::move(table);
 
@@ -114,7 +194,7 @@ void HashTable<T, Hasher>::Resize() {
 
 
 void run(std::istream &input, std::ostream &output) {
-    HashTable<std::string, StringHasher> hash_table;
+    HashTable<std::string, DoubleHashProbing<StringHasher>> hash_table;
 
     char op;
 
@@ -142,6 +222,8 @@ void run(std::istream &input, std::ostream &output) {
             }
         }
     }
+
+    output << hash_table << std::endl;
 }
 
 
